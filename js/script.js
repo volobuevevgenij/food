d2f764b1e3be9a0c0cc2970efb26e57f8b6d75fe
Evgen <alllan@mail.ru>
2022-03-23 13:50:52 +0300

    tooo

--- js/script.js
+++ js/script.js
@@ -285,10 +285,7 @@ window.addEventListener('DOMContentLoaded', ()  => {
         }, 4000);
     }
     
-    fetch('db.json')
-        .then(data => data.json())
-        .then(res => console.log(res));
 
-        
+
     
 });

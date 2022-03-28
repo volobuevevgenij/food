284c81fae7ae4510a2d3ba4009d7b805cf4b7dc0
Evgen <alllan@mail.ru>
2022-03-28 14:49:26 +0300

    addslide

--- js/script.js
+++ js/script.js
@@ -294,43 +294,105 @@ window.addEventListener('DOMContentLoaded', ()  => {
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           total = document.querySelector('#total'),
-          current = document.querySelector('#current');
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
     let slideIndex = 1;
+    let offset = 0;
 
-    showSlides(slideIndex); // 5. ствим предел по фото (1/4)
-    if (slides.length < 10){
+    if(slides.length < 10){
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
     }else{
-        total.textContact = slides.length;
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
     
-    function showSlides(n){ // 1. проверяем клики на предел фото
-        if (n > slides.length){
+    slidesField.style.width = 100 * slides.length+ '%'; //1 устанавливаем шириу контейнера для слайдов
+    slidesField.style.display = 'flex'; // 3. устанавливаем  горизонтально слайды
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width; // 2 устанавливаем одинаковую ширину для каждого сайда
+    });
+
+    next .addEventListener('click', () => {
+        if(offset == +width.slice(0, width.length-2)*(slides.length - 1)){
+            offset = 0;
+        }else{
+            offset += +width.slice(0, width.length - 2);
+        }
+        
+        slidesField.style.transform = `translateX(-${offset}px)`;
+        if(slideIndex== slides.length){
             slideIndex = 1;
+        }else{
+            slideIndex ++;
         }
-        if (n < 1){
-            slideIndex = slides.length;
-        }
-        
-        slides.forEach(item => item.style.display = "none");// 2. скрываем все фото
-        slides[slideIndex - 1].style.display = "block"; //3. открываем нужное фото
-        
-        if(slides.length < 10){ // 6. ставим 1ю цивру с 1/4
+
+        if(slides.length < 10){
             current.textContent = `0${slideIndex}`;
         }else{
             current.textContent = slideIndex;
         }
-    }
-
-    function plusSlides(n){ // 4. отправляю значение при нажати на кнопки
-        showSlides(slideIndex += n)
-    }
+    });
 
     prev.addEventListener('click', () => {
-        plusSlides(-1);
+        if(offset == 0){
+            offset = +width.slice(0, width.length -2)*(slides.length - 1);
+        }else{
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if(slideIndex == 1 ){
+            slideIndex = slides.length;
+        }else{
+            slideIndex--;
+        }
+        if(slides.length < 10){
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
     });
-    next.addEventListener('click', () => {
-        plusSlides(1);
-    });
+    // showSlides(slideIndex); // 5. ствим предел по фото (1/4)
+    // if (slides.length < 10){
+    //     total.textContent = `0${slides.length}`;
+    // }else{
+    //     total.textContact = slides.length;
+    // }
+    
+    // function showSlides(n){ // 1. проверяем клики на предел фото
+    //     if (n > slides.length){
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1){
+    //         slideIndex = slides.length;
+    //     }
+        
+    //     slides.forEach(item => item.style.display = "none");// 2. скрываем все фото
+    //     slides[slideIndex - 1].style.display = "block"; //3. открываем нужное фото
+        
+    //     if(slides.length < 10){ // 6. ставим 1ю цивру с 1/4
+    //         current.textContent = `0${slideIndex}`;
+    //     }else{
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function plusSlides(n){ // 4. отправляю значение при нажати на кнопки
+    //     showSlides(slideIndex += n)
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1);
+    // });
+    // next.addEventListener('click', () => {
+    //     plusSlides(1);
+    // });
     
 });
